6b3df52f5cca384f919c760c115552e7101204fd
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-01-25 13:44:36 +0200

    added logic for back button

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,6 +15,12 @@ class SettingsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //Тут сделал кликабельным весь контейнер
+        val buttonBackSettings = findViewById<LinearLayout>(R.id.settings_screen)
+        buttonBackSettings.setOnClickListener {
+            val buttonBackIntent = Intent(this, MainActivity::class.java)
+            startActivity(buttonBackIntent)
         }
     }
 }
