3c1764abc9f155ee97902b91d41c87ed3cb4cfe2
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-01-25 13:44:49 +0200

    added logic for main screen

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +14,28 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.search)
+        val clickSearchButton: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val searchButtonIntent = Intent(this@MainActivity,SearchActivity::class.java)
+                startActivity(searchButtonIntent)
+            }
+        }
+        searchButton.setOnClickListener(clickSearchButton)
+
+        val mediaButton = findViewById<Button>(R.id.media)
+        mediaButton.setOnClickListener {
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+
+        }
+
+        val settingsButton = findViewById<Button>(R.id.settings)
+        settingsButton.setOnClickListener{
+            val settingsIntent = Intent(this,SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
     }
 }
