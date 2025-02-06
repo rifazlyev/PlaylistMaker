cf94d4543ba7e6df9b0c09014b868ad536c09443
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-02-06 10:02:00 +0200

    implemented actions for buttons

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.LinearLayout
@@ -27,19 +28,38 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         //Тут кликабельна и сама 'стрелочка'
-        val buttonBack= findViewById<ImageButton>(R.id.back_button_settings_screen)
+        val buttonBack = findViewById<ImageButton>(R.id.back_button_settings_screen)
         buttonBack.setOnClickListener {
             val buttonBackIntent = Intent(this, MainActivity::class.java)
             startActivity(buttonBackIntent)
         }
 
         val buttonShareApp = findViewById<LinearLayout>(R.id.share_app)
-        buttonShareApp.setOnClickListener{
-            val message = "https://practicum.yandex.ru/android-developer"
+        buttonShareApp.setOnClickListener {
+            val message = getString(R.string.course_link)
             val intent = Intent(Intent.ACTION_SEND)
             intent.putExtra(Intent.EXTRA_TEXT, message)
             intent.type = "text/plain"
             startActivity(Intent.createChooser(intent, "Choose the app"))
+        }
+
+        val buttonSupport = findViewById<LinearLayout>(R.id.support)
+        buttonSupport.setOnClickListener {
+            val themeText = getString(R.string.theme_text)
+            val bodyMessage = getString(R.string.body_message)
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.user_email)))
+            intent.putExtra(Intent.EXTRA_SUBJECT, themeText)
+            intent.putExtra(Intent.EXTRA_TEXT, bodyMessage)
+            startActivity(intent)
+        }
+
+        val buttonPrivacyPolicy = findViewById<LinearLayout>(R.id.privacy_policy)
+        buttonPrivacyPolicy.setOnClickListener {
+            val url = Uri.parse(getString(R.string.privacy_link))
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
         }
     }
 }
