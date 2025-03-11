69166da69bb17bc60dd70bef032693500c241d4c
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-03-11 15:56:22 +0200

    added fun formatTrackTime

--- app/src/main/java/com/example/playlistmaker/viewHolder/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/viewHolder/TrackViewHolder.kt
@@ -10,6 +10,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.model.Track
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackName = itemView.findViewById<TextView>(R.id.track_title)
@@ -25,10 +27,14 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         ).toInt()
     }
 
+    private fun formatTrackTime(durationInMillis: Long): String{
+        return SimpleDateFormat("mm:ss", Locale.getDefault()).format(durationInMillis)
+    }
+
     fun bind(track: Track) {
         trackName.text = track.trackName
         artistName.text = track.artistName
-        trackTime.text = track.trackTime
+        trackTime.text = formatTrackTime(track.trackTime)
 
         val radiusPx = dpToPx(2F, itemView.context)
 
