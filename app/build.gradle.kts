db6981adade1f77fe4b2be6083a4858cfd9f1043
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-03-11 15:53:56 +0200

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 
 dependencies {
     implementation (libs.glide)
+    implementation(libs.retrofit)
+    implementation (libs.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
