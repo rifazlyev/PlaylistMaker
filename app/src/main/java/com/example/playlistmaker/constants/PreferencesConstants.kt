2f375596695a2fe4ca4b5e0fb0add11ad6d7ae28
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-03-22 13:56:50 +0200

    added object for sharedPreferences key

---
+++ app/src/main/java/com/example/playlistmaker/constants/PreferencesConstants.kt
@@ -0,0 +1,8 @@
+package com.example.playlistmaker.constants
+
+object PreferencesConstants {
+    const val PLAYLIST_PREFERENCES = "playlist_prefs"
+
+    //Ключи для хранения данных
+    const val SEARCH_HISTORY_KEY = "key_for_search_history"
+}
