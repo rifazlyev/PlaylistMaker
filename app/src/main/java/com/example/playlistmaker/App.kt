2aafb27b8ca3ccebb50ee42e6f783782ed6a4fb4
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-03-23 15:09:44 +0200

    added App class

---
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -0,0 +1,31 @@
+package com.example.playlistmaker
+
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.playlistmaker.constants.PreferencesConstants.APP_THEME_KEY
+import com.example.playlistmaker.constants.PreferencesConstants.PLAYLIST_PREFERENCES
+
+class App : Application() {
+    private var darkTheme: Boolean = false
+    lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate() {
+        super.onCreate()
+        sharedPreferences = getSharedPreferences(PLAYLIST_PREFERENCES, MODE_PRIVATE)
+        darkTheme = sharedPreferences.getBoolean(APP_THEME_KEY, false)
+        switchTheme(darkTheme)
+
+    }
+
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        darkTheme = darkThemeEnabled
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+}
