650ae402f21e996a469f96a7b138c3b7ff690080
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-04-03 23:00:53 +0300

    added new activity

---
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -0,0 +1,29 @@
+package com.example.playlistmaker
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PlayerActivity : AppCompatActivity() {
+    private lateinit var buttonBack: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        buttonBack = findViewById(R.id.back_button_player_screen)
+        buttonBack.setOnClickListener{
+            finish()
+        }
+
+
+    }
+}
