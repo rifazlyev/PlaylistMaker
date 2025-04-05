f7fdc1fc2115ec432aa052188acca4c9d8f715fc
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-04-05 20:33:49 +0300

    added utils object

---
+++ app/src/main/java/com/example/playlistmaker/uiUtils/UiUtils.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker.uiUtils
+
+import android.content.Context
+import android.util.TypedValue
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+object UiUtils {
+    fun dpToPx(dp: Float, context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics
+        ).toInt()
+    }
+
+    fun formatTrackTime(durationInMillis: Long): String {
+        return SimpleDateFormat("mm:ss", Locale.getDefault()).format(durationInMillis)
+    }
+}
