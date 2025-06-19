d0c4947071458182d884ad99ad39169ac0a09054
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-06-19 21:42:02 +0300

    refactor app class

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -1,37 +1,16 @@
 package com.example.playlistmaker
 
 import android.app.Application
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatDelegate
-import com.example.playlistmaker.common.PreferencesConstants.APP_THEME_KEY
-import com.example.playlistmaker.common.PreferencesConstants.PLAYLIST_PREFERENCES
+import com.example.playlistmaker.data.ThemeControllerImpl
 
 class App : Application() {
+    private lateinit var themeController : ThemeControllerImpl
     private var darkTheme: Boolean = false
-    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate() {
         super.onCreate()
-        sharedPreferences = getSharedPreferences(PLAYLIST_PREFERENCES, MODE_PRIVATE)
-        darkTheme = sharedPreferences.getBoolean(APP_THEME_KEY, false)
-        switchTheme(darkTheme)
-    }
-
-    fun switchTheme(darkThemeEnabled: Boolean) {
-        darkTheme = darkThemeEnabled
-        sharedPreferences.edit()
-            .putBoolean(APP_THEME_KEY, darkThemeEnabled)
-            .apply()
-        AppCompatDelegate.setDefaultNightMode(
-            if (darkThemeEnabled) {
-                AppCompatDelegate.MODE_NIGHT_YES
-            } else {
-                AppCompatDelegate.MODE_NIGHT_NO
-            }
-        )
-    }
-
-    fun isDarkThemeEnabled():Boolean{
-        return darkTheme
+        themeController = ThemeControllerImpl(this)
+        darkTheme = themeController.isDarkThemeEnabled()
+        themeController.switchTheme(darkTheme)
     }
 }
