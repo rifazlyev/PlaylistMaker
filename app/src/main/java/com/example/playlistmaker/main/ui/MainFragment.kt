3baba77c17d05466d8a6b3d41779574619138d81
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-07-08 22:56:40 +0300

    transfer MediaActivity to Fragment

--- app/src/main/java/com/example/playlistmaker/main/ui/MainFragment.kt
+++ app/src/main/java/com/example/playlistmaker/main/ui/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentMainBinding
+import com.example.playlistmaker.media.ui.MediaFragment
 import com.example.playlistmaker.settings.ui.SettingsFragment
 
 class MainFragment : Fragment() {
@@ -30,7 +31,10 @@ class MainFragment : Fragment() {
 
 
         binding.media.setOnClickListener {
-
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.rootFragmentContainerView, MediaFragment.newInstance())
+                .addToBackStack(null)
+                .commit()
         }
 
         binding.settings.setOnClickListener {
