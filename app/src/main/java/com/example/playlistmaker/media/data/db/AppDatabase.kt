a0e6ada128ba17b5e89f6a39c217a1ee7f3d3293
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-08-23 16:24:10 +0300

    update db version and Dao

--- app/src/main/java/com/example/playlistmaker/media/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/playlistmaker/media/data/db/AppDatabase.kt
@@ -2,9 +2,11 @@ package com.example.playlistmaker.media.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.playlistmaker.media.data.db.entity.PlaylistEntity
 import com.example.playlistmaker.media.data.db.entity.TrackEntity
 
-@Database(version = 1, entities = [TrackEntity::class])
+@Database(version = 2, entities = [TrackEntity::class, PlaylistEntity::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getTrackDao(): TrackDao
+    abstract fun getPlaylistDao(): PlaylistDao
 }
