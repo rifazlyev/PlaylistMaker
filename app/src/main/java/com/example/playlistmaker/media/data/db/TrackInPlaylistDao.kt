dd3d2d683f3d99c12309896533d7f334d7572bfb
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-09-01 22:26:47 +0300

    new method in dao for getting tracks in db

--- app/src/main/java/com/example/playlistmaker/media/data/db/TrackInPlaylistDao.kt
+++ app/src/main/java/com/example/playlistmaker/media/data/db/TrackInPlaylistDao.kt
@@ -3,10 +3,15 @@ package com.example.playlistmaker.media.data.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.playlistmaker.media.data.db.entity.TrackInPlaylistEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TrackInPlaylistDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertTrackToPlaylist(trackInPlaylistEntity: TrackInPlaylistEntity): Long
+
+    @Query("SELECT * FROM TRACK_IN_PLAYLIST")
+    fun getAllTracks(): Flow<List<TrackInPlaylistEntity>>
 }
